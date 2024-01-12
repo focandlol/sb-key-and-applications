a04b40b22d1c17e866504cfd7df144f9cbe7aa5a
focandlol <focandlol@naver.com>
2024-01-12 17:39:43 +0900

    section05 auto configuration end

---
+++ autoconfig/memory-v1/src/main/java/memory/Memory.java
@@ -0,0 +1,29 @@
+package memory;
+
+public class Memory {
+
+    private Long used;
+
+    private Long max;
+
+    public Memory(Long used, Long max) {
+        this.used = used;
+        this.max = max;
+    }
+
+    public Long getUsed() {
+        return used;
+    }
+
+    public Long getMax() {
+        return max;
+    }
+
+    @Override
+    public String toString() {
+        return "Memory{" +
+                "used=" + used +
+                ", max=" + max +
+                '}';
+    }
+}
